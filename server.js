44e8d776d04fcda15f6d0ee000abbf25ef0ff25b
narz <i.arzaut@itecriocuarto.org.ar>
2021-09-16 02:41:59 -0300

    add https

--- server.js
+++ server.js
@@ -2,14 +2,17 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const https = require('https')
+const fs = require('fs')
 // Import routes
 const authRouter = require("./routes/auth");
 const postulantesRouter = require("./routes/postulantes");
 const sendEmailRouter = require("./routes/send-email");
 // Define port
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3030;
 // Instantiate server
 const app = express();
+
 // Middlewares
 /*var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));*/
@@ -25,6 +28,12 @@ app.get("/", (req,res) => {
 	res.send('hola')
 })
 
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+https.createServer({
+  key: fs.readFileSync('cert/server.key'),
+  cert: fs.readFileSync('cert/server.cert')
+}, app).listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
 })
+/*app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})*/
